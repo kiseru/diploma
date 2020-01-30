bd156543d32667457ae9ba7424bd6c690b03a766
Alexandr Kiselev <kis.alex97@gmail.com>
2020-01-30 21:02:37 +0300

    Added getting more information about employers

--- main.py
+++ main.py
@@ -13,18 +13,45 @@ def find_select_of_institutes():
         f.write(str(soup.find('select', attrs={'name': 'p_id'})))
 
 
-kfu_value_in_search_form = 0
-kfu_main_page_url = 'https://kpfu.ru/main_page'
-r = requests.post(kfu_main_page_url, data={'p_id': 6895, 'p_sub': 7860, 'p_order': 1})
-soup = BeautifulSoup(r.text, 'html.parser')
+def get_employers():
+    kfu_value_in_search_form = 0
+    kfu_main_page_url = 'https://kpfu.ru/main_page'
+    r = requests.post(kfu_main_page_url, data={'p_id': 6895, 'p_sub': 7860, 'p_order': 1})
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-#ss_content > div.area_width > div > div:nth-child(2) > table:nth-child(5) > tbody > tr > td
-employers_count = int(soup.select('#ss_content > div.area_width > div > div:nth-child(2) > table > tr > td')[0].string.split(' ')[1])
+    employers_count = int(
+        soup.select('#ss_content > div.area_width > div > div:nth-child(2) > table > tr > td')[0].string.split(' ')[1]
+    )
 
-r = requests.post(kfu_main_page_url, data={'p_id': 6895, 'p_sub': 7860, 'p_order': 1, 'p_rec_count': employers_count})
-soup = BeautifulSoup(r.text, 'html.parser')
-with open('anchors.json', 'w') as f:
+    r = requests.post(
+        kfu_main_page_url,
+        data={'p_id': 6895, 'p_sub': 7860, 'p_order': 1, 'p_rec_count': employers_count}
+    )
+    soup = BeautifulSoup(r.text, 'html.parser')
     anchors = [i.find('a') for i in soup.find_all('tr', attrs={'class': 'konf_tr'})]
     anchors = {anchor['href']: anchor.string for anchor in anchors if anchor is not None}
-    print(anchors)
-    f.write(json.dumps(anchors, sort_keys=True, indent=4))
+    return anchors
+
+
+def get_employer_info(employer_link, employer_name):
+    soup = BeautifulSoup(requests.get(employer_link).text, 'html.parser')
+    try:
+        employer_info = soup.select('.left_width > div > div > div')[1].select('div')
+    except IndexError:
+        return {}
+    data = {
+        'link': employer_link,
+    }
+    lst = [(employer_info[i], employer_info[i + 1]) for i in range(0, len(employer_info), 2)]
+    for (key, value) in lst:
+        values = [i.get_text().replace('\n', '') for i in value.select('.menu_list > .li_spec')]
+        data[key.get_text()] = values
+
+    return employer_name, data
+
+
+employers = get_employers()
+result = [get_employer_info(employer_link, employer_name) for (employer_link, employer_name) in employers.items()]
+result = [i for i in result if len(i) > 0]
+with open('employers.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps({key: value for (key, value) in result}, ensure_ascii=False, sort_keys=True, indent=4))
