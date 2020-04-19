373a74f9d216c8b745a832b824f1745f1bb59c9f
kiseru <kis.alex97@gmail.com>
2020-04-19 22:21:12 +0300

    feat: добавлен парсинг статей из Google Scholar

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests
 from bs4 import BeautifulSoup, Tag
 import json
 
+EMPLOYERS_JSON = 'employers.json'
+HTTPS_SCHOLAR_GOOGLE_RU = 'https:/scholar.google.ru'
+JSON_INDENT = 2
+HREF = 'href'
+GOOGLE_SCHOLAR = 'Google scholar'
+LINKS = 'Ссылки'
 HTML_PARSER = 'html.parser'
 KFU_MAIN_PAGE_URL = 'https://kpfu.ru/main_page'
 KFU_EMPLOYERS_PAGE_URL = 'https://kpfu.ru/staff/sotrudniki-kfu'
@@ -115,10 +121,10 @@ def get_links(employer_soup):
     link_rows = employer_soup.select('.right_block > div:first-child table tr')
     links = {row.contents[0].string.strip()[:-1]: row.contents[1].string
              for row in link_rows}
-    return {'Ссылки': links}
+    return {LINKS: links}
 
 
-if __name__ == '__main__':
+def parse_kpfu():
     employers = get_employers()
     result = [
         get_employer_info(link, name) for (link, name) in employers.items()
@@ -129,6 +135,50 @@ if __name__ == '__main__':
             {key: value for (key, value) in result},
             ensure_ascii=False,
             sort_keys=True,
-            indent=4,
+            indent=JSON_INDENT,
         )
         f.write(json_encoded)
+
+
+def parse_scholar_articles():
+    with open(EMPLOYERS_JSON, 'r', encoding='utf-8') as fp:
+        employers = json.load(fp)
+
+    employers_with_scholar_names = [
+        employer
+        for employer, employer_info in employers.items()
+        if GOOGLE_SCHOLAR in employer_info[LINKS]
+    ]
+
+    for employer in employers_with_scholar_names:
+        employer_scholar_link = employers[employer][LINKS][GOOGLE_SCHOLAR]
+        scholar_response = requests.get(employer_scholar_link)
+        bs = BeautifulSoup(scholar_response.text, HTML_PARSER)
+        bs_articles = bs.select("#gsc_a_t .gsc_a_tr")
+        articles = [parse_article(bs_article) for bs_article in
+                    bs_articles]
+        employers[employer][GOOGLE_SCHOLAR] = articles
+        with open(EMPLOYERS_JSON, 'w', encoding='utf-8') as fp:
+            json.dump(employers, fp, ensure_ascii=False, indent=JSON_INDENT)
+
+
+def parse_article(bs_article):
+    try:
+        bs_article_title = bs_article.select_one('.gsc_a_t a')
+        bs_article_cited_by = bs_article.select_one('.gsc_a_c a')
+        bs_article_year = bs_article.select_one('.gsc_a_y span')
+        return {
+            'title': str(bs_article_title.string),
+            'link': HTTPS_SCHOLAR_GOOGLE_RU + bs_article_title['data-href'],
+            'cited_by': int(bs_article_cited_by.string)
+            if bs_article_cited_by.string is not None else 0,
+            'year': int(bs_article_year.string)
+            if bs_article_year.string is not None else -1,
+        }
+    except AttributeError:
+        return None
+
+
+if __name__ == '__main__':
+    parse_kpfu()
+    parse_scholar_articles()
